e2c6078802c6caa9b495a781a18b4e2cef926350
Oscar Rieken <oriekenjr@gmail.com>
2012-07-01 00:47:49 -0400

    added a check for new search functionality

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#etsy_rspec_taza_tutorial
+#Etsy RSpec Taza Tutorial
 [![Build Status](https://secure.travis-ci.org/hammernight/etsy_rspec_taza_tutorial.png)](http://travis-ci.org/hammernight/etsy_rspec_taza_tutorial)
--- spec/isolation/etsy/advanced_search_spec.rb
+++ spec/isolation/etsy/advanced_search_spec.rb
@@ -15,11 +15,15 @@ describe "Advanced search" do
 
   context "from the top navigation" do
     it "lets me change the item type" do
-      etsy.etsy_header.search_drop_down.click
-      etsy.etsy_header.search_type("Supplies").when_present.click
+
+      if etsy.etsy_header.search_drop_down.exists?
+        etsy.etsy_header.search_drop_down.click
+        etsy.etsy_header.search_type("Supplies").when_present.click
+      end
+
       etsy.etsy_header.search_text.set "belt"
       etsy.etsy_header.search_button.click
-      etsy.search_results_page.search_filter_radio("Supplies").checked?.should be_true
+      etsy.search_results_page.search_results_message.text.should include("belt")
     end
   end
 end
